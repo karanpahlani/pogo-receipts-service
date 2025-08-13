cf5091d9e5b82af470656d7d2c7f7023e639e4d5
karanpahlani <pahlani@gmail.com>
2025-08-12 23:51:45 -0700

    drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5444,
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'password',
-    database: process.env.DB_NAME || 'pogo_data',
-    ssl: false,
+    url: process.env.DATABASE_URL || 'postgresql://postgres:password@localhost:5444/pogo_data',
   },
 });
